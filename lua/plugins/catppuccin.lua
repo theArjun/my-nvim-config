6fdd864cf6293c24a1f33129bca30178c617f63c
thearjun <33110520+theArjun@users.noreply.github.com>
2026-03-03 13:42:02 +0545

    feat: use catpuccin

---
+++ lua/plugins/catppuccin.lua
@@ -0,0 +1,85 @@
+return {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    config = function()
+        require("catppuccin").setup({
+            flavour = "mocha", -- latte, frappe, macchiato, mocha
+            background = {
+                light = "latte",
+                dark = "mocha",
+            },
+            transparent_background = true,
+            show_end_of_buffer = false,
+            term_colors = true,
+            dim_inactive = {
+                enabled = false,
+                shade = "dark",
+                percentage = 0.15,
+            },
+            no_italic = false,
+            no_bold = false,
+            no_underline = false,
+            styles = {
+                comments = { "italic" },
+                conditionals = { "italic" },
+                loops = {},
+                functions = {},
+                keywords = {},
+                strings = {},
+                variables = {},
+                numbers = {},
+                booleans = {},
+                properties = {},
+                types = {},
+                operators = {},
+            },
+            color_overrides = {},
+            custom_highlights = {},
+            integrations = {
+                cmp = true,
+                gitsigns = true,
+                nvimtree = true,
+                treesitter = true,
+                notify = true,
+                mini = {
+                    enabled = true,
+                    indentscope_color = "",
+                },
+                telescope = {
+                    enabled = true,
+                },
+                barbar = true,
+                indent_blankline = {
+                    enabled = true,
+                    colored_indent_levels = false,
+                },
+                mason = true,
+                native_lsp = {
+                    enabled = true,
+                    virtual_text = {
+                        errors = { "italic" },
+                        hints = { "italic" },
+                        warnings = { "italic" },
+                        information = { "italic" },
+                    },
+                    underlines = {
+                        errors = { "underline" },
+                        hints = { "underline" },
+                        warnings = { "underline" },
+                        information = { "underline" },
+                    },
+                    inlay_hints = {
+                        background = true,
+                    },
+                },
+                neotree = true,
+                which_key = true,
+            },
+        })
+        
+        -- Set colorscheme
+        vim.cmd.colorscheme("catppuccin")
+    end,
+}
+
